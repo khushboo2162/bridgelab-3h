452e7df3c3468f6355fea0fb87dd84179525c5be
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-03 15:22:40 +0530

    Added level 1 folder from local STRING directory

---
+++ STRING/level 1/StringComparison.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class StringComparison {
+    public static boolean compareUsingCharAt(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.next();
+        System.out.print("Enter second string: ");
+        String str2 = sc.next();
+
+        boolean customResult = compareUsingCharAt(str1, str2);
+        boolean builtInResult = str1.equals(str2);
+
+        System.out.println("Custom Comparison Result: " + customResult);
+        System.out.println("Built-in equals() Result: " + builtInResult);
+
+        if (customResult == builtInResult) {
+            System.out.println("Both methods give the same result.");
+        } else {
+            System.out.println("Results differ.");
+        }
+        sc.close();
+    }
+}
