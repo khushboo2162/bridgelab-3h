322d405869015327bf136b563fc94de808033924
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-10 21:24:03 +0530

    Added Department.java to UniversityFacultyDepartmentModel

---
+++ ObjectModelingProblems/UniversityFacultyDepartmentModel/Department.java
@@ -0,0 +1,11 @@
+public class Department {
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Department: " + name);
+    }
+}
