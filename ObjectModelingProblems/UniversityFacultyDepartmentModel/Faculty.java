2f43650391c58bf4186f8f25fc7d337630e66860
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-10 21:23:43 +0530

    Added Faculty.java to UniversityFacultyDepartmentModel

---
+++ ObjectModelingProblems/UniversityFacultyDepartmentModel/Faculty.java
@@ -0,0 +1,18 @@
+public class Faculty {
+    private String name;
+    private String specialization;
+
+    public Faculty(String name) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void displayInfo() {
+        System.out.println("Faculty: " + name + ", Specialization: " + specialization);
+    }
+
+    public void display() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'display'");
+    }
+}
