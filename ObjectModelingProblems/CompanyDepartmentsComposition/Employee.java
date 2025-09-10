0c5fb9a64aec23846270af62148cf785e5c8399b
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-10 21:06:14 +0530

    Added Employee.java file

---
+++ ObjectModelingProblems/CompanyDepartmentsComposition/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id + ", Name: " + name);
+    }
+}
