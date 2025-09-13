12542f2420423758024266f79faca92137165abb
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 09:26:44 +0530

    Added SmartHomeTest java fil\e

---
+++ Inheritance/SingleInheritance/SmartHomeDevices/SmartHomeTest.java
@@ -0,0 +1,6 @@
+public class SmartHomeTest {
+    public static void main(String[] args) {
+        Thermostat livingRoomThermostat = new Thermostat("TH123", "ON", 22.5);
+        livingRoomThermostat.displayStatus();
+    }
+}
