9705cfbcbaca08d128c9ea078c02e3c6f296871d
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 09:07:02 +0530

    Added  Intern java file

---
+++ Inheritance/EmployeeManagementSystem/Intern.java
@@ -0,0 +1,14 @@
+public class Intern extends Employee {
+    private int durationMonths;
+
+    public Intern(String name, int id, double salary, int durationMonths) {
+        super(name, id, salary);
+        this.durationMonths = durationMonths;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + durationMonths + " months");
+    }
+}
