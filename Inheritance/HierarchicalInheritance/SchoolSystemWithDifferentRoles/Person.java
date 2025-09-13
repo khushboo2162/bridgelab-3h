9389129c9bc457191a518265d67f58a28acb8a91
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 09:53:23 +0530

    Added Person java file

---
+++ Inheritance/HierarchicalInheritance/SchoolSystemWithDifferentRoles/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public void displayRole() {
+        System.out.println("Role: Generic Person");
+    }
+}
