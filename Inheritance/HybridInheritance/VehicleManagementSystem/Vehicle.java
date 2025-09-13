8fa58ff78958c55cedd9dfad98ae8b55807f722c
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 10:05:56 +0530

    Added Vehicle java file

---
+++ Inheritance/HybridInheritance/VehicleManagementSystem/Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+    protected int maxSpeed;
+    protected String model;
+
+    public Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+
+    public void displayInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
