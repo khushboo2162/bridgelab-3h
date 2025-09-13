5e9f3c3b3f15b7b4538fc45325660bf0a4a269b4
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 09:23:27 +0530

    Added LibraryTest java file

---
+++ Inheritance/SingleInheritance/LibraryManagement/LibraryTest.java
@@ -0,0 +1,12 @@
+public class LibraryTest {
+    public static void main(String[] args) {
+        Author book1 = new Author(
+            "The Java Journey",
+            2023,
+            "Khushboo Sharma",
+            "A passionate developer exploring object-oriented programming."
+        );
+
+        book1.displayInfo();
+    }
+}
