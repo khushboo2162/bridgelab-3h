7cfac8cb5caaa1ddcc4e69988819943fef898339
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 08:59:00 +0530

    Added Dog java file

---
+++ Inheritance/AnimalHierarchy/Dog.java
@@ -0,0 +1,11 @@
+// Subclass: Dog
+class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof woof!");
+    }
+}
