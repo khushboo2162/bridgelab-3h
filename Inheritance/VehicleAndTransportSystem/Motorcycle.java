38cda28c2ed62b4c031513943b68a1878cac353d
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2025-09-13 09:17:05 +0530

    Added Motorcycle java filr

---
+++ Inheritance/VehicleAndTransportSystem/Motorcycle.java
@@ -0,0 +1,15 @@
+
+public class Motorcycle extends Vehicle {
+    private boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + (hasCarrier ? "Yes" : "No"));
+    }
+}
